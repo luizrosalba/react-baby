1025446ffb8fcbeb47f87eb0ed4b85d4f89e7081
luiz <luizrosalba@gmail.com>
2022-01-17 23:05:13 -0300

    first Commit

---
+++ src/util/baby-links.js
@@ -0,0 +1,7 @@
+export const babyLinks = {
+   links: [
+    'https://images.unsplash.com/photo-1548897969-80be2924dc9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    'https://images.unsplash.com/photo-1489760176169-fd3d32805239?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+
+]}  
